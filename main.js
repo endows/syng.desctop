c74da91d5f2949016725f07e00ba20e4124d5f60
endows <you@example.com>
2015-07-20 23:03:57 +0900

    ok

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
-var updater = require('electron-updater')
 
 
 require('crash-reporter').start();
@@ -15,30 +14,13 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  updater.on('ready', function () {
-      // mainWindow = new BrowserWindow({width: 800, height: 600})
-      // mainWindow.loadUrl('file://' + __dirname + '/index.html')
-      // mainWindow.openDevTools({detach:true})
-      // mainWindow.on('closed', function() {
-      //     mainWindow = null;
-      // })
-      updater.on('updateRequired', function () {
-          app.quit();
-      })
-      updater.on('updateAvailable', function () {
-        console.log('ok')
-          mainWindow.webContents.send('update-available');
-      })
-      updater.start()
-
-  })
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 800
   });
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
